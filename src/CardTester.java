6a47298bcb87e3bf3297129dfc8870e7b3cd969e
ZachP30 <zacherytphillips30@gmail.com>
2020-02-15 15:08:51 -0500

    Image loading and panel

--- src/CardTester.java
+++ src/CardTester.java
@@ -1,5 +1,40 @@
 import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+import java.awt.*;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JEditorPane;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class CardTester {
 
  public static void main(String[] args) {
@@ -115,6 +150,35 @@ public class CardTester {
 	// score = canPlace(outsideSextets.get(0).getInternalStack().get(1), topLeft[0], score)[1];
 	// System.out.println(Arrays.toString(canPlace(outsideSextets.get(0).getInternalStack().get(2), topLeft[0], score)));
 	// score = canPlace(outsideSextets.get(0).getInternalStack().get(1), topLeft[0], score)[1];
+
+  /* START OF MAKING THE GUI*/
+  //1. Create the frame.
+  JFrame frame = new JFrame("Crescent Solitare");
+
+  //2. Optional: What happens when the frame closes?
+  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+  //3. Create components and put them in the frame.
+  //...create emptyLabel...
+
+  Image image = Toolkit.getDefaultToolkit().createImage("logo.png");
+  JPanel outterPiles = new JPanel(new BorderLayout()) {
+          @Override
+          protected void paintComponent(Graphics g) {
+              super.paintComponent(g);
+              g.drawImage(image, 0, 0, null);
+          }
+      };
+
+  frame.add(outterPiles);
+  //frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+
+  //4. Size the frame.
+  frame.pack();
+
+  //5. Show it.
+  frame.setVisible(true);
+
 
  } //main()
 
