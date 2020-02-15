53ce7fbab25262157df7d07f555dbb3610b78ecd
ZachP30 <42448700+ZachP30@users.noreply.github.com>
2020-02-15 04:00:21 -0500

    Card logic and setup of the backend game

---
+++ src/Card.java
@@ -0,0 +1,46 @@
+
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+public class Card{
+
+private int value;
+private String suit;
+private String type;
+
+public Card(){
+  value = 0;
+  suit = null;
+  type = null;
+} //default Constructor
+
+public Card(int value, String suit, String type){
+this.value = value;
+this.suit = suit;
+this.type = type;
+} //Constructor
+
+public void setValue(int value){
+  this.value = value;
+}//setValue
+
+public void setSuit(String suit){
+  this.suit = suit;
+}//setSuit
+
+public void setType(String type) {
+	this.type = type;
+}
+
+public int getValue() {
+	return value;
+}
+public String getSuit() {
+	return suit;
+}
+
+public String getType() {
+	return type;
+}
+
+
+}//End Card Class
